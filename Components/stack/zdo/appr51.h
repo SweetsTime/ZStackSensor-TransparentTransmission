293c1597dab32c356d756f4663892ba6f60d24e8
Stellar Embers <102133103+AngelicFall@users.noreply.github.com>
2023-05-21 01:43:11 +0800

    Add files via upload

---
+++ Components/stack/zdo/appr51.h
@@ -0,0 +1,14 @@
+#ifndef __APPR51_H__
+#define __APPR51_H__
+
+extern void ZigBee_C_R_E_Engineering_A(void);
+extern void ZigBee_C_R_E_Engineering_B(void);
+extern void ZigBee_C_R_E_Engineering_C(void);
+extern void ZigBee_C_R_E_Engineering_D(void);
+extern void ZigBee_C_R_E_Engineering_E(void);
+extern void ZigBee_C_R_E_Engineering_F(void);
+extern void ZigBee_C_R_E_Engineering_G(void);
+extern void ZigBee_C_R_E_Engineering_H(void);
+extern void ZigBee_C_R_E_Engineering_I(void);
+
+#endif
